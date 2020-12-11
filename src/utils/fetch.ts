74257e59bc01cac36886094c8ab5eb3fd8c69c06
Brayan <haider.escalona@gmail.com>
2020-12-11 18:04:20 -0500

    Cambio peticion

---
+++ src/utils/fetch.ts
@@ -0,0 +1,28 @@
+const API_URL =
+  "https://2ngrcmozhk.execute-api.us-east-2.amazonaws.com/default/sendMessage?email=";
+
+const apiFetch = async (url: string, method: "GET") => {
+  try {
+    const dataResult = await fetch(API_URL + url, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method,
+    });
+    if (!dataResult.ok) {
+      const { message } = await dataResult.json();
+      throw message;
+    }
+    return await dataResult.json();
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const sendMessage = async (message: string) => {
+  try {
+    const resolve = await apiFetch(message, "GET");
+  } catch (err) {
+    throw err;
+  }
+};
